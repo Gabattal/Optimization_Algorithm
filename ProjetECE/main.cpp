d47774964541337dc309b20444ec4ec08e81ae92
Gab <gab-attal@hotmail.fr>
2019-04-18 09:44:13 +0200

    Menu

--- ProjetECE/main.cpp
+++ ProjetECE/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
-#include "graphe.h"
 #include <vector>
+#include <cstdlib>
+#include "graphe.h"
 #include "svgfile.h"
 #include "couleur.h"
 #define UNIT 76.0f
@@ -12,22 +13,42 @@ int main()
 {
     Svgfile svgout("output.svg", MAP_WIDTH, MAP_HEIGHT);
     int choix=1;
+    std::string nom;
+    std::string poids;
 
-    while(choix!=3)
+       do
     {
+
         std::cout<<"Que voulez-vous faire ?"<<std::endl;
         std::cout<<"1 - Choisir un graphe"<<std::endl;
         std::cout<<"2 - Executer un algorithme sur le graphe"<<std::endl;
         std::cout<<"3 - Quitter"<<std::endl;
-        std::cin>>choix;
-    }
+        std::cin>>(choix);
 
-   graphe g{"manhattan.txt","manhattan_weights_0.txt",&svgout};
-   g.afficher_graph();
-   g.Prim(1, 0);
+        system("cls");
+        if(choix==1)
+        {
+        std::cout<<"Choisir un fichier de base"<<std::endl;
+        std::cin>>(nom);
+
+        system("cls");
+        std::cout<<"Choisir un poids"<<std::endl;
+        std::cin>>(poids);
+        }
+        if (choix==2)
+        {
+
+        }
+
+    }while(choix!=3);
+
+    graphe g{nom+".txt",nom+"_weights_"+poids+".txt",&svgout};
+            g.afficher_graph();
+            g.Prim(1, 0);
+            g.generateSvg();
+            choix=0;
    std::cout<<std::endl;
    //g.Prim(1, 1);
-   g.generateSvg();
 
    //graphe_weight {"broadway_weight_0.txt"};
     //std::cout<<tableau[0];
